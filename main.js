2389e33e461307999b69735a8cbf5fbc62484f0b
Andrew Cox <drew.r.cox@gmail.com>
2022-03-07 13:53:25 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const input = 'Please rescue me from Seaworld!';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+for (let i = 0; i < str.length; i++) {
+    const letter = str[i].toLowerCase();
+    if (letter === 'e' || letter === 'u') {
+      resultArray.push(letter);
+    }
+    for (let j = 0; j < vowels.length; j++) {
+      const vowelLetter = vowels[j];
+      if (letter === vowelLetter) {
+        resultArray.push(letter);
+      }
+    }
+  }
